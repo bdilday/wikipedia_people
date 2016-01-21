899c16d6b900e365575bc4a33acb37d49ceb537d
Ben Dilday <ben@gravityjack.com>
2016-01-21 09:56:23 -0800

    pass integer to top10.sh to print a number different than 10

--- top10.sh
+++ top10.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-cat data/*csv | sed 's/,/ /g' | awk '{print $3, $0}' | sort -nr | head -10
+if [ "$#" -gt 0 ];then
+    ntop=$1;
+else
+    ntop=10
+fi
+
+cat data/*csv | sed 's/,/ /g' | awk '{print $3, $0}' | sort -nr | head -${ntop}
+    
